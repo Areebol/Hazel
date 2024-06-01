6557cdb03f50609b4e4542bbf92682eb75593030
areebol <12657507+areebol@user.noreply.gitee.com>
2024-06-02 00:07:41 +0800

    Added PreCompiledHeader

--- Hazel/src/Hazel/Events/KeyEvent.h
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -2,12 +2,10 @@
 
 #include "Event.h"
 
-#include <sstream>
-
 namespace Hazel {
 
 	class HAZEL_API KeyEvent : public Event
-	{
+	{ 
 	public:
 		inline int GetKeyCode() const { return m_KeyCode; }
 
