317b2fcf7b6ab33f1435cf00df6da2b002e16faa
Areebol <103118945+Areebol@users.noreply.github.com>
2024-06-18 16:47:54 +0800

    Started Renderer2D class

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -3,6 +3,7 @@
 
 #include "Platform/OpenGL/OpenGlRendererAPI.h"
 #include "Platform/OpenGL/OpenGLShader.h"
+#include "Renderer2D.h"
 
 namespace Hazel {
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
@@ -27,6 +28,7 @@ namespace Hazel {
 	void Renderer::Init()
 	{
 		RenderCommand::Init();
+		Renderer2D::Init();
 	}
 
 	void Renderer::OnWindowResize(uint32_t width, uint32_t height)
