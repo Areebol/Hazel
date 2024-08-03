d2ebbc4b3cdc4c9fff518f25b5ec24c049ae6adf
Areebol <103118945+Areebol@users.noreply.github.com>
2024-08-03 14:42:22 +0800

    Fixed bug when creating new scene

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -80,6 +80,7 @@ namespace Hazel {
 	void SceneHierarchyPanel::SetContext(const Ref<Scene>& context)
 	{
 		m_Context = context;
+		m_SelectionContext = {};
 	}
 
 	void SceneHierarchyPanel::OnImGuiRender()
